23a478888c78c49b006f0f558d7cdedae9b8b92e
Adel <bordbariadel@gmail.com>
2020-06-05 05:11:24 +0430

    add post author

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # delete user -> delete their posts too
+    slug = models.SlugField()
+    # delete user -> delete their posts too
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
