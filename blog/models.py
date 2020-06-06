79e7142aec1d6d16de0ee94ac250ae179a529291
Adel <bordbariadel@gmail.com>
2020-06-07 04:14:19 +0430

    add some icons

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.template.defaultfilters import slugify
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
@@ -13,12 +15,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
-    
+
     def get_absolute_url(self):
-        # go to the url named 'details' and pass in self.pk
-        return reverse('details', kwargs={'slug': self.slug})
-    
-    def save(self, *args, **kwargs): # pass in args just in case
-        if not self.id: # if it's a new post, set slug
+        # in order  go to the url named 'details' and pass in self.pk
+        # return reverse('details', kwargs={'slug': self.slug})
+        # in order to redirect to home page after writing a new post
+        return reverse('home')
+
+    def save(self, *args, **kwargs):  # pass in args just in case
+        if not self.id:  # if it's a new post, make a slug
+            candid = slugify(self.title)
+            duplicates = len(Post.objects.filter(slug=candid))
+            if duplicates > 0:  # if already exists
+                for i in range(duplicates):
+                    candid += '_re' + str(i+1)
             self.slug = slugify(self.title)
         return super(Post, self).save(*args, **kwargs)
