723f7e25db6f2a0a3d924ea7da1d73933138b18b
Adel Bordbari <blackdead263@yahoo.com>
2020-09-11 06:03:48 +0430

    tired

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import ModelFormMixin
 from .models import Post, Category
 from .forms import *
+
 
 class post_list_view(ListView):
     model = Post
@@ -23,15 +25,18 @@ class add_post_view(CreateView):
     form_class = PostForm
     # fields = ['author', 'title', 'body']  # '__all__' for all elements
 
+
 class add_category_view(CreateView):
     model = Category
     template_name = "add_category.html"
     fields = '__all__'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        #name the context, then say what attr to append into it
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        # name the context, then say what attr to append into it
         context['categories'] = Category.objects.all().values('name')
         return context
+
 
 class update_post_view(UpdateView):
     model = Post
