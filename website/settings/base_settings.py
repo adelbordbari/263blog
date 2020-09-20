757753be14bd074118ea29c0a26c144fd2fe0430
Adel Bordbari <blackdead263@yahoo.com>
2020-09-20 04:35:03 +0430

    add whitenoise

--- website/settings/base_settings.py
+++ website/settings/base_settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = INSTALLED_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -128,8 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "assets"),)
+
+#whitenoise storage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFileStorage"
 
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
